4d49a6275ddba91d79d6834dd024affbcd0ffaa0
BelaNepal <belanepal2025@gmail.com>
2025-07-25 16:54:45 +0545

    proj text

--- frontend/app/components/Navbar.js
+++ frontend/app/components/Navbar.js
@@ -48,7 +48,7 @@ export default function Navbar() {
         style={{ fontSize: scrolled ? "1.2rem" : "1.8rem", zIndex: 60 }}
       >
         <span className="typing-text text-blue-700 font-bold whitespace-nowrap">
-          Bela Nepal IMS
+          Project - Bela IMS
         </span>
       </div>
 
