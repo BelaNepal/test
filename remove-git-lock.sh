6df7f619d69a3f88c7d0943a9bd811dc6195767b
Ashish T <ashishh459@gmail.com>
2025-07-28 15:39:04 +0545

    Added .sh to lock files

---
+++ remove-git-lock.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script to remove git lock files safely
+
+LOCKFILE=".git/refs/remotes/origin/main.lock"
+
+if [ -f "$LOCKFILE" ]; then
+  echo "Removing lock file: $LOCKFILE"
+  rm "$LOCKFILE"
+  echo "Lock file removed."
+else
+  echo "No lock file found at $LOCKFILE"
+fi
