1e5c38e8ff9b7f0db76c7567853e3b9c363492b3
BelaNepal <belanepal2025@gmail.com>
2025-08-04 16:54:13 +0545

    fix

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -18,17 +18,18 @@ export default function Home() {
       {/* Left-aligned Welcome Section */}
       <div className="relative z-10 max-w-xl text-left">
         <h1 className="text-5xl font-extrabold text-gray-900 mb-4 leading-tight">
-          Welcome<span className="text-blue-600"> Bela-IMS</span>
+          Welcome to<span className="text-blue-600"> Bela-IMS</span>
         </h1>
         <p className="text-lg text-gray-700 mb-6">
           Start managing your project with ease. Access your dashboard, track progress, and explore tools built just for you.
         </p>
         <Link
           href="/login"
-          className="inline-block bg-blue-600 text-white px-6 py-2 rounded-lg shadow hover:bg-blue-700 transition"
+          className="relative inline-block bg-blue-600 text-white px-6 py-2 rounded-xl shadow-md transition-all duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg hover:bg-[#ef7e1f] focus:outline-none focus:ring-2 focus:ring-blue-400"
         >
-          Go to Login
-        </Link>
+  Go to Login
+</Link>
+
       </div>
     </div>
   );
